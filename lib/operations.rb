6c840d91a873dd7d44f84af239598ec42cf1667d
krishnendu basak <krisauz17@yahoo.com>
2022-05-11 15:42:27 +1000

    operators lab is done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,31 @@
-def unsafe?(speed)
+#speed_a = 79
+#speed_b = 35
+#speed_c = 50
 
+speed = [79, 35, 50]
+
+def unsafe(speed)
+    if speed < 40 || speed > 60
+     puts "it is unsafe"
+    else
+    puts " it is safe"
+    end
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe(speed)
+    speed < 40 || speed  > 60 ? "unsafe" : "safe"
 end
-	
 
+#def not_safe(speed_b)
+#   speed_b < 40 || speed_b > 60 ? "it is unsafe" : "it is safe"
+#end
+#def not_safe(speed_c)
+#    speed_c < 40 || speed_c > 60 ? "it is unsafe" : "it is safe"
+#end
 
+#unsafe(speed)
+
+not_safe(speed)
+
+#not_safe(speed_b)
+#not_safe(speed_c)
